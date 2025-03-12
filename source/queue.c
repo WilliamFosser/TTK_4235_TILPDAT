8f32529cc33f0081f3632d496de1362d0efc784a
Tom Inge <tibjorke@stud.ntnu.no>
2025-03-12 19:12:43 +0100

    Changed queue system

--- source/queue.c
+++ source/queue.c
@@ -5,26 +5,34 @@
 
 
 void new_queue(Queue *queue) {
-    for (uint8_t i = 0; i < N_FLOORS; i++) {
-        queue->prioritized_orders[i] = false;
-        queue->unprioritized_orders[i] = false;
+    queue->order_type[0] = 1; //Up, Position = ButtonType
+    queue->order_type[1] = -1; //Down
+    queue->order_type[2] = 0; //Cab
+    for (uint8_t i = 0; i < 4; i++) {
+        queue->orders[i] = NULL;
     }
 };
 
-void add_order(Queue *queue, uint8_t floor, bool is_cab_order) {
-    if (is_cab_order) {
-        queue->prioritized_orders[floor] = true;
-        printf("Added prioritized order \n");
-    } else {
-        queue->unprioritized_orders[floor] = true;
-        printf("Added unprioritized order \n");
+void add_order(Queue *queue, uint8_t floor, ButtonType button_type, Direction direction) {
+    elevio_buttonLamp(floor, button_type, 1);
+    if(queue->orders[floor] == NULL) {
+        queue->orders[floor] = &(queue->order_type[button_type]);
     }
+    else if (button_type == BUTTON_CAB) {
+        queue->orders[floor] = &(queue->order_type[button_type]);
+        printf("Added cab order \n");
+    } else{
+        if (*(queue->orders[floor]) != queue->order_type[button_type]) { //If there's an order in the opposite direction, order doesnt matter
+            queue->orders[floor] = &(queue->order_type[2]);
+            printf("Added cab order \n");}
+        else {
+            queue->orders[floor] = &(queue->order_type[button_type]);
+            printf("Added down order  in direction %d\n", queue->order_type[button_type]);}
 };
 
 
 void pop_order(Queue *queue, uint8_t floor) {
-    queue->prioritized_orders[floor] = false;
-    queue->unprioritized_orders[floor] = false;
+    queue->orders[floor] = NULL;
 
     elevio_buttonLamp(floor, BUTTON_HALL_DOWN, 0);
     elevio_buttonLamp(floor, BUTTON_HALL_UP, 0);
@@ -34,8 +42,7 @@ void pop_order(Queue *queue, uint8_t floor) {
 
 void pop_all_orders(Queue *queue) {
     for (uint8_t floor = 0; floor < N_FLOORS; floor++) {
-        queue->prioritized_orders[floor] = false;
-        queue->unprioritized_orders[floor] = false;
+        queue->orders[floor] = NULL;
 
         elevio_buttonLamp(floor, BUTTON_HALL_DOWN, 0);
         elevio_buttonLamp(floor, BUTTON_HALL_UP, 0);
